7e9379b2b7ccc4ea6e941847569e49c92477928c
EnderTheCoder <1991455223@qq.com>
2022-01-06 09:55:18 +0800

    现在购买可以选择物品与数量

--- src/main/java/com/ender/globalmarket/command/UserCommand.java
+++ src/main/java/com/ender/globalmarket/command/UserCommand.java
@@ -33,22 +33,28 @@ public class UserCommand implements CommandExecutor {
         UUID uuid = player.getUniqueId();
         switch (args[0]) {
             case "buy": {
-
+                //调试代码，记得移除
                 Vault.addVaultCurrency(uuid, 0.1);
 
-                double neededMoney = MarketEconomy.getBuyingPrice();
+                Material itemToBuy = Material.matchMaterial(args[1]);
+                if (itemToBuy == null ) {
+                    sender.sendMessage(ChatColor.RED + "你输入的物品不存在，请在物品名称前加命名空间。如原版物品minecraft:..., 或者机械动力物品create:...");
+                    break;
+                }
+
+                double neededMoney = MarketEconomy.getBuyingPrice(itemToBuy, Integer.parseInt(args[2]));
 
                 if (Vault.checkCurrency(uuid) >= neededMoney) {
                     Vault.subtractCurrency(uuid, neededMoney);
 
-                    ItemStack item = new ItemStack(Material.DRAGON_EGG);
-                    item.setAmount(1); // 设置为 32 个
+                    ItemStack itemToGive = new ItemStack(itemToBuy);
+                    itemToGive.setAmount(1); // 设置为 32 个
 
                     PlayerInventory inventory = player.getInventory();
 
-                    inventory.addItem(item);
+                    inventory.addItem(itemToGive);
 
-                    sender.sendMessage(ChatColor.GREEN + "你成功购买了物品");
+                    sender.sendMessage(ChatColor.GREEN + String.format("你成功购买了物品 %s", itemToBuy.name()));
                     sender.sendMessage(String.format(ChatColor.YELLOW + "你的账户余额为 %s", Vault.checkCurrency(uuid)));
                 } else {
                     sender.sendMessage(ChatColor.RED + String.format("你的账户余额为 %s ， 本次购买需要 %s ， 试试如何去搞点钱来", Vault.checkCurrency(uuid), neededMoney));
--- src/main/java/com/ender/globalmarket/economy/MarketEconomy.java
+++ src/main/java/com/ender/globalmarket/economy/MarketEconomy.java
@@ -1,13 +1,15 @@
 package com.ender.globalmarket.economy;
 
+import org.bukkit.Material;
+
 public class MarketEconomy {
 
 
-    public static double getBuyingPrice(){
+    public static double getBuyingPrice(Material item, int count) {
         return 0.1;
     }
 
-    public static double getSellingPrice(){
+    public static double getSellingPrice() {
         return 0.1;
     }
 
